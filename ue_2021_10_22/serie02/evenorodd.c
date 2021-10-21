7efb09996e4b5232d7d58ca063def5533b6edc2f
VoxelPigit commit -m Initial <verblockt@gmail.com>
2021-10-21 21:40:12 +0200

    Don't use function for evenorodd.c

--- ue_2021_10_22/serie02/evenorodd.c
+++ ue_2021_10_22/serie02/evenorodd.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 
-int evenorodd(int n) {
-    return !(n & 1); // n & 1 == (n % 2)
-} 
-
 int main() {
-
     unsigned int n = 0;
     printf("n = ");
     scanf("%u", &n);
 
-    printf("n is %s\n", evenorodd(n) ? "even" : "odd");
+//                      (n & 1)
+    printf("n is %s\n", (n % 2) ? "odd" : "even");
 
     return 0;
 }
+
+// int evenorodd(int n) {
+//     return !(n & 1); // n & 1 == (n % 2)
+// } 
+
+// int main() {
+
+//     unsigned int n = 0;
+//     printf("n = ");
+//     scanf("%u", &n);
+
+//     printf("n is %s\n", evenorodd(n) ? "even" : "odd");
+
+//     return 0;
+// }
